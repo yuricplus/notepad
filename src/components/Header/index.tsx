f10a0657934f2d85a0f582ce54e25e650a71a27c
yuricplus <yurigamer16@gmail.com>
2021-07-09 17:21:33 -0300

    Feature: layout header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaFacebookF, FaLinkedinIn, FaGithub } from 'react-icons/fa';
 
 import './style.scss'
 
@@ -7,8 +8,29 @@ export const Header: React.FC = () => {
         <header className="header">
             <div className="header__logo">
                 <a href="/" rel="noopener noreferrer" className="header__logo__link">
-                   <h1>notepad online</h1>
+                   <h1><strong className="bold">Notepad</strong>.online</h1>
                 </a>
+            </div>
+            <div className="header__links">
+                <ul className="header__links__box">
+                    <li className="header__links__box__text">Resources</li>
+                    <li className="header__links__box__text">Blog</li>
+                    <li className="header__links__box__text">
+                        <a href="http://" target="_blank" rel="noopener noreferrer">
+                          <FaFacebookF color="#0049ff"/>
+                        </a>
+                    </li>
+                    <li className="header__links__box__text">
+                        <a href="http://" target="_blank" rel="noopener noreferrer">
+                          <FaLinkedinIn color="#0049ff"/>
+                        </a>
+                    </li>
+                    <li className="header__links__box__text">
+                        <a href="http://" target="_blank" rel="noopener noreferrer">
+                          <FaGithub color="#0049ff"/>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </header>
     )
