4f1594874e802d232bf767469d2586b14de374c8
yuricplus <yurigamer16@gmail.com>
2021-07-09 17:22:04 -0300

    Feature: layout home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,7 +6,6 @@ import './style.scss'
 export const Home: React.FC = () => {
     const history:any = useHistory();
     const [idCreated, setIdCreated] = useState('');
-    const [idAccess, setIdAccess] = useState('');
 
     function redirect():void {
         history.push(`/note/${idCreated}`);
@@ -15,37 +14,34 @@ export const Home: React.FC = () => {
     return (
         <section className="content">
             <div className="content__item">
-                <h2>create a new ID</h2>
+                <h2>ID</h2>
                 <form action="" className="content__item__form">
                     <input 
                       type="text" placeholder="htpps://notepad.com/yourid" 
                       className="content__item__form__input"
                       onChange={(value) => setIdCreated(value.target.value)}
                     />
-                    <button 
-                      className="content__item__form__button"
-                      onClick={(e)=> {
-                          e.preventDefault()
-                          redirect()
-                      }}>create</button>
-                </form>
-            </div>
-            <div className="content__item">
-                <h2>acess any ID</h2>
-                <form action="" className="content__item__form">
-                    <input 
-                      type="text" placeholder="htpps://notepad.com/yourid" 
-                      className="content__item__form__input" 
-                      autoComplete="disable"
-                      onChange={(value) => setIdAccess(value.target.value)}
-                    />
-                    <button 
-                      className="content__item__form__button"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        redirect();
-                      }}
-                      >access</button>
+                    <div className="content__item__form__cnt">
+                      <button 
+                          className="content__item__form__cnt__button__primary"
+                          onClick={(e)=> {
+                              e.preventDefault()
+                              if(!idCreated) {
+                                return;
+                              }
+                              redirect()
+                          }}> + New</button> 
+                        <span className="content__item__form__cnt__or">or</span>
+                      <button 
+                        className="content__item__form__cnt__button__secondary"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          if(!idCreated) {
+                            return;
+                          }
+                          redirect();
+                        }}>Access</button>
+                    </div>
                 </form>
             </div>
         </section>
